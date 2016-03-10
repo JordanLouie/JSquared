44895af65c77e1ae6132aec4676b3eabf39cdbf5
ThatAznKid <jchen47@stuy.edu>
2016-03-10 11:47:39 -0500

    Testing Mechanism implemented

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -36,13 +36,28 @@ public class MergeSortTester {
     public static void main( String[] args ) {
 	int[] arr0 = {0}; 
 	int[] arr1 = new int[10];
-    
-	MergeSort.printArray(arr0);
-	populate(arr1); 
-	MergeSort.printArray(arr1);  
-	//add the thing to keep track of time and run a couple of hundred of thousands of times and yeah
-	MergeSort.printArray(MergeSort.sort(arr1));
+	long TStart = 0; 
+	long TEnd = 0;
+	long TPassed = 0; 
+	long TAverage = 0;
 
+	MergeSort.printArray(arr0);
+	
+	for (int i = 0 ; i < 100 ; i++) { 
+	    populate(arr1); 
+	    MergeSort.printArray(arr1);  
+	    //add the thing to keep track of time and run a couple of hundred of thousands of times and yeah
+	    TStart = System.currentTimeMillis(); 
+	    System.out.println("Time Start: " + TStart);
+	    MergeSort.printArray(MergeSort.sort(arr1));
+	    TEnd = System.currentTimeMillis(); 
+	    System.out.println("Time End: " + TEnd);
+	    TPassed = TEnd - TStart; 
+	    System.out.println (TPassed); 
+	    TAverage += TPassed; 
+	} 
+	TAverage /= 100; 
+	System.out.println(TAverage);
     }//end main
 
 }//end class
